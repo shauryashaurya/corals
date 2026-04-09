cb16bab8ae55e829ac74504ff47af75e0b72f371
Shaurya Agarwal <shauryashaurya@gmail.com>
2026-04-09 21:29:11 +0530

    updating vite path to unlock GitHub pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
+// tell Vite to use relative paths for your assets so that it builds correctly regardless of the sub-directory it is hosted in.
+// thus allowing one to publish this as a GitHub Pages site as well. 
 export default defineConfig({
   plugins: [react()],
+  base: './', // this line to fix GitHub Pages asset paths
 })
